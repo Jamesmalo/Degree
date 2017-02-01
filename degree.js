bb4c1234ab584dbd0afea7aef4aabad6153cc77e
Jamesmalo <9jmalo@stpatrick.org>
2017-02-01 11:26:21 -0600

    start (day 1)

---
+++ degree.js
@@ -0,0 +1,29 @@
+const Matrix = require("matrix-js");
+
+let A = Matrix([
+    [1,1,0,1,0],
+    [1,0,1,0,0],
+    [0,1,0,0,0],
+    [1,0,0,0,1],
+    [0,0,0,1,0]
+]);
+//Code here
+function degree(mtrx,v){
+  let sum = 0;
+  let degreelist = []
+    for(i=0;i<degreelist.lenght;i++){
+    let sum=sum+A(i);
+  }
+  return sum;
+}
+
+function degreecentrality(){
+ let neighbors = 0;
+
+}
+
+//Challenge
+
+//Run test code with ctrl+shift+p  then type in "script"
+console.log(degree(A,3));
+console.log(degreecentrality(A,2));
